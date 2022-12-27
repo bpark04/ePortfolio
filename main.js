ba3b08b684adddab335dd5769cf20ea251ca48be
bpark <byungsun1819@gmail.com>
2022-12-26 23:07:44 -0800

    all requiremnts implemented, more touches needed

--- main.js
+++ main.js
@@ -4,8 +4,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+// https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
 
-// Transform navbar to be transparent when scrolled down
+// Transform navbar when scrolled down
 let navbar = document.querySelector('#navbar');
 let navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -14,6 +15,14 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar__dark');
   }
+});
+
+// Transform home to be transparent when scrolled down
+let home = document.querySelector('.home__container');
+let homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  let opacity = 1 - (window.scrollY / homeHeight);
+  home.style.opacity = opacity;
 });
 
 // to scroll to the target section
@@ -34,3 +43,34 @@ navbarMenu.addEventListener('click', scrollTo);
 
 let contactMe = document.querySelector('.home__contact');
 contactMe.addEventListener('click', scrollTo);
+
+// Projects
+let workBtnContainer = document.querySelector('.work__categories');
+let projectContainer = document.querySelector('.work__projects');
+let projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  let filter = e.target.dataset.filter;
+  if (filter === null) {
+    return;
+  }
+
+  let active = document.querySelector('.category__btn.active');
+  active.classList.remove('active');
+  e.target.classList.add('active');
+
+
+  projectContainer.classList.add('animation-out');
+  
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('animation-out');
+  }, 300)
+
+});
